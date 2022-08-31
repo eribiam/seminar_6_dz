43e42981e77b2da377c9059905980fcfbf608eb0
dmitriy <boss.vede@mail.ru>
2022-08-31 15:49:48 +0700

    second exercise

--- Program.cs
+++ Program.cs
@@ -44,12 +44,20 @@
 // Console.WriteLine("]");
 // Console.WriteLine($"Колличество чисел больше нуля = {count}");
 
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-//y = k1(x + b1)
-//y = k2(x + b2)
 
-
+// Console.WriteLine("Введите значение b1 ");
+// float b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение k1 ");
+// float k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение b2 ");
+// float b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение k2 ");
+// float k2 = Convert.ToInt32(Console.ReadLine());
+// float x = (b2 - b1)/(k1 - k2);
+// float y = k1 * x + b1;
+// System.Console.WriteLine($"Точка пересечения ( {x} , {y} )");
 
 
 // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
